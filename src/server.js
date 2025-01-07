4657d9ae9a1ea7b0a3503bec789c690b6d1d0cf3
igorferreira007 <ferreiraigor007@gmail.com>
2025-01-07 19:44:09 -0300

    First commit

---
+++ src/server.js
@@ -0,0 +1,11 @@
+import http from "node:http"
+
+import { jsonHandler } from "./middlewares/jsonHandler.js"
+import { routerHandler } from "./middlewares/routeHandler.js"
+
+async function listener(request, response) {
+  await jsonHandler(request, response)
+  routerHandler(request, response)
+}
+
+http.createServer(listener).listen(3333)
